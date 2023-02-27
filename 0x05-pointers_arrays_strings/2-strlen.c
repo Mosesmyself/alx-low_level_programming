9b35fb7e8168e6c32727c52e3528c891719b7a28
Olusakin Akinola <olusakinakinola@gmail.com>
2023-02-27 20:00:34 +0000

    Task 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string.
+ * @s: string.
+ * Return: return the lenght.
+ */
+int _strlen(char *s)
+
+{
+	int lenght = 0;
+
+	while (*str != '0')
+	{
+		lenght++;
+		s++;
+	}
+
+	return (lenght0);
+}
