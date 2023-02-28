efe2177a05dd38bb6e7548b64cc7ed6c483f4978
Olusakin Akinola <olusakinakinola@gmail.com>
2023-02-28 16:56:27 +0000

    Task 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,16 @@
  * print_rev - prints a string, in reverse, followed by a new line.
  * @s: string to be printed in reverse.
  */
+
 void print_rev(char *s)
+
 {
 	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
 	for (i = i - 1; i >= 0; i--)
-		putchar(s[i]);
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
