d6f41f50fdc62d48ff35679b52cded99a31dd5bb
Olusakin Akinola <olusakinakinola@gmail.com>
2023-02-28 11:01:31 +0000

    Task 5-reverse_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,11 +2,11 @@
 
 /**
  * rev_string - reverses a string.
- * s: string in reverse.
+ * @s: string in reverse.
  */
 void rev_string(char *s)
 {
-	char rv = s[0];
+	char rev = s[0];
 	int c = 0;
 	int i;
 
@@ -15,8 +15,8 @@ void rev_string(char *s)
 	for (i = 0; i < c; i++)
 	{
 		c--;
-		rv = s[i];
+		rev = s[i];
 		s[i] = s[c];
-		s[c] = rv;
+		s[c] = rev;
 	}
 }
