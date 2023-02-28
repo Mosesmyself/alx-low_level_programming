0d99cfd2be99eb9bbbe5818e44ef2f29d8674484
Olusakin Akinola <olusakinakinola@gmail.com>
2023-02-28 10:57:13 +0000

    Task 5-reverse_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,7 @@
  */
 void rev_string(char *s)
 {
-	char rev = s[0];
+	char rv = s[0];
 	int c = 0;
 	int i;
 
@@ -15,8 +15,8 @@ void rev_string(char *s)
 	for (i = 0; i < c; i++)
 	{
 		c--;
-		rev = s[i];
+		rv = s[i];
 		s[i] = s[c];
-		s[c] = rev;
+		s[c] = rv;
 	}
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - prints every other character of a string.
  * @str: input.
  */
+
 void puts2(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (str[i] != '\0')
+	while (count >= 0)
 	{
-		if (i % 2 == 0)
+		if (str[count] == '\0')
 		{
-			putchar(str[i]);
+			_putchar('\n');
+			break;
 		}
-		i++;
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-	putchar('\n');
-}
-		
+
+}	
