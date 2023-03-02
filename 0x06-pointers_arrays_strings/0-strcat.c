4a8c400028241ab5fa502bd4394b1c136a83c1ec
Olusakin Akinola <olusakinakinola@gmail.com>
2023-03-02 03:56:35 +0000

    Task 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,9 @@
  * Return: Returns a pointer to the resulting string dest.
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 
 {
-
-	char *src[] = "New ";
-	char *dest[] = "string";
 
 	strcat(*dest, *src);
 	return (dest);
