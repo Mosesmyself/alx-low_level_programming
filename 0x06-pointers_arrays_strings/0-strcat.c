92e3a60da6e2e4f229faafd2677775c4b690019c
Olusakin Akinola <olusakinakinola@gmail.com>
2023-03-02 04:21:22 +0000

    Task 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,18 @@ char *_strcat(char *dest, char *src)
 
 {
 
-	strcat(*dest, *src);
+	int a, b;
+
+	a = 0;
+	while (dest[a] != '\0')
+		a++;
+	{
+		b = 0;
+		while (src[b] != '\0')
+			dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[b] = '\0';
 	return (dest);
 }
