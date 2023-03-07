c54b05686ad58eb058fbc1a4bb2f9b5de0a57ec8
Olusakin Akinola <olusakinakinola@gmail.com>
2023-03-07 04:09:12 +0000

    fix2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,17 @@
  * *_strchr - locates a character in a string.
  * @c: input char.
  * @s: input string.
- * Return: s or NULL.
+ * Return: 0.
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int x = 0;
+
+	for (; s[x] >= '\0'; x++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
+		if (s[x] == c)
+			return (&s[x]);
 	}
-	return ('\0');
+	return (0);
 }
